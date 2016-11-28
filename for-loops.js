259d0370a2bcc41b98c82f58515eb83202155a40
Luke Woodward <luke.wdwrd@gmail.com>
2016-11-28 12:01:33 -0600

    looped over the object to print each key of the key:value pair to the console

--- for-loops.js
+++ for-loops.js
@@ -35,3 +35,30 @@ var americanPresidents = ["George Washington", "John Adams", "Thomas Jefferson",
 for (var i = 0; i < americanPresidents.length; i++) {
   console.log(americanPresidents[i] + " was a president of the USA");
 }
+
+
+// ########################################
+
+console.log("<---- 4 --->")
+
+// Use can use another kind of for loop to iterate over objects.
+// Iterate over the object below and use console.log() to output
+// the names of the keys in the object.
+
+var antSpecies = {
+  argentine: {},
+  army: {},
+  bigHeaded: {},
+  black: {},
+  bull: {},
+  carpenter: {},
+  crazy: {},
+  fire: {},
+  glider: {},
+  honeyPot: {},
+  jackJumper: {}
+}
+
+for (var i in antSpecies) {
+  console.log(i);
+}
