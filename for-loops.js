88fd746eaab27d6abf5f4fbc61a0e894dd027a1e
Luke Woodward <luke.wdwrd@gmail.com>
2016-11-28 11:39:49 -0600

    added for loop that logs every 10th number from 5 to 125

---
+++ for-loops.js
@@ -0,0 +1,7 @@
+// // Write a for loop that uses a counter variable initialized at 5
+// and then increments it by 10 every time it executes. Use console.log()
+// to output the value of the counter variable each time through the loop.
+// Stop execution of the loop if the counter variable's value is greater than 120.
+for (var i = 5; i < 126; i += 10) {
+  console.log(i);
+}
