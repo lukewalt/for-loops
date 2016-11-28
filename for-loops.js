bb7f56e8d7c8e94e13ac5d4e6c51b1472df3ea86
Luke Woodward <luke.wdwrd@gmail.com>
2016-11-28 11:46:00 -0600

    added for loop that divides the counter by 2 starting with 4096 ending at 1

--- for-loops.js
+++ for-loops.js
@@ -2,6 +2,19 @@
 // and then increments it by 10 every time it executes. Use console.log()
 // to output the value of the counter variable each time through the loop.
 // Stop execution of the loop if the counter variable's value is greater than 120.
+console.log("<---- 1 --->")
+
 for (var i = 5; i < 126; i += 10) {
+  console.log(i);
+}
+
+console.log("<---- 2 --->")
+
+// Write a for loop with a counter variable initialized at 4096.
+// Each time the loop executes divide the counter variable's value by 2.
+// Use console.log() to output its value every time.
+// When the counter variable's value is 1, stop execution.
+
+for (var i = 4096; i >= 1 ; i /= 2) {
   console.log(i);
 }
